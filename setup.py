b22d3e777714977ebf15bf05d85c012fa6f0e4d1
Matheus Vaz Manzke <matheus@tuco.com.br>
2024-05-23 11:28:30 -0300

    finished model analysis

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='An analysis of steam pages data.',
-    author='Matheus Vaz Manzke',
-    license='',
+    version="0.1.0",
+    description="An analysis of steam pages data.",
+    author="Matheus Vaz Manzke",
+    license="",
 )
